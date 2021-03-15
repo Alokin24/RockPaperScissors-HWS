c52654f0ea55514a755f0802da6e12a7ae063ce7
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-03-15 19:45:48 +0100

    Initial Commit

---
+++ RockPaperScissors-HWS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors-HWS
+//
+//  Created by Nikola Anastasovski on 15.3.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors-HWS/RockPaperScissors_HWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissors_HWSApp.swift
+//  RockPaperScissors-HWS
+//
+//  Created by Nikola Anastasovski on 15.3.21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissors_HWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
